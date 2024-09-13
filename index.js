4ab056ad1caf3d0c076a8bdb9d16311ef79f2224
Teekay <tkmaile96@gmail.com>
2024-09-13 12:33:19 +0200

    added songs to my array

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const songs = [
     { title: "Come and Get Your Love", artist: "Redbone", genre: "Rock" },
     { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
     { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
+    {title: ""}
     // Feel free to add even more songs
 ];
 
